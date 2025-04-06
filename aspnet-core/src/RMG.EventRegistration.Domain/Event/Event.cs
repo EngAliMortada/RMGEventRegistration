12a514f9322a57a4fd57741717208c1517931944
AliMortada <ali.mortada@yandex.com>
2025-04-06 14:42:41 +0300

    feat(domain): create enitties and aggregate roots

---
+++ aspnet-core/src/RMG.EventRegistration.Domain/Event/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Identity;
+
+namespace RMG.EventRegistration.Event;
+
+public class Event : FullAuditedAggregateRoot<Guid>
+{
+    public string Name { get; private set; }
+    public int Capacity { get; private set; }
+    public bool IsOnline { get; private set; }
+    public DateTimeOffset StartDate { get; private set; }
+    public DateTimeOffset EndDate { get; private set; }
+    public Guid OrganizerId { get; private set; }
+    public string Link { get; private set; }
+    public string Location { get; private set; }
+    public bool IsActive { get; private set; }
+
+    #region navigation
+    public virtual IdentityUser? Organizer { get; set; }
+    #endregion navigation
+}
---
+++ aspnet-core/src/RMG.EventRegistration.Domain/Event/EventRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Identity;
+
+namespace RMG.EventRegistration.Event;
+
+public class EventRegistration : AuditedEntity<Guid>
+{
+    public Guid EventId { get; set; }
+    public Guid UserId { get; set; }
+
+    #region navigation properties
+    public virtual Event Event { get; protected set; }
+    public virtual IdentityUser User { get; protected set; }
+    #endregion navigation properties
+
+}
