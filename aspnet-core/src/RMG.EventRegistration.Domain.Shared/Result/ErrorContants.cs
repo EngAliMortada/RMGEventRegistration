ebb8f3aadac0346f5cdafcc45504323b1ce97901
AliMortada <ali.mortada@yandex.com>
2025-04-06 14:56:23 +0300

    feat(domain-shared): implement result pattern

---
+++ aspnet-core/src/RMG.EventRegistration.Domain.Shared/Result/ErrorContants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMG.EventRegistration.Result
+{
+    public struct ErrorContants
+    {
+        public const string Invalid = " is invalid";
+        public const string InvalidCode = "invalid";
+
+        public const string Required = " is required";
+        public const string RequiredCode = "required";
+
+        public const string IsNull = " is null";
+        public const string IsNullCode = "isnull";
+    }
+}
---
+++ aspnet-core/src/RMG.EventRegistration.Domain.Shared/Result/Result.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMG.EventRegistration.Result
+{
+    public class Result
+    {
+
+        public bool Succeeded { get; init; }
+        public string ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool IsFailed => !Succeeded;
+        public bool Warning { get; init; }
+        public bool IsWarned => this.Warning;
+    }
+
+    public class SucceededResult : Result
+    {
+        public string SuccessMessage { get; set; }
+        public SucceededResult() : base()
+        {
+            Succeeded = true;
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; set; }
+    }
+
+    public class SucceededResult<T> : Result<T>
+    {
+        public SucceededResult() : base()
+        {
+            Succeeded = true;
+        }
+    }
+
+    public class FailedResult : Result
+    {
+        public FailedResult()
+        {
+            Succeeded = false;
+        }
+
+        public FailedResult(string errorCode, string errorMessage)
+        {
+            Succeeded = false;
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+        }
+    }
+
+    public class FailedResult<T> : Result<T>
+    {
+        public FailedResult() : base()
+        {
+            Succeeded = false;
+        }
+
+        public FailedResult(string errorCode, string errorMessage) : base()
+        {
+            Succeeded = false;
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+        }
+    }
+
+    public class FailedNullResult<T> : FailedResult<T>
+    {
+        public FailedNullResult(string objectName)
+        {
+            ErrorCode = $"{objectName}{ErrorContants.IsNullCode}";
+            ErrorMessage = $"{objectName} {ErrorContants.IsNull}";
+        }
+    }
+
+    public class FailedNullResult : FailedResult
+    {
+        public FailedNullResult(string objectName)
+        {
+            ErrorCode = $"{objectName}{ErrorContants.IsNullCode}";
+            ErrorMessage = $"{objectName} {ErrorContants.IsNull}";
+        }
+    }
+
+    public class FailedInvalidParamResult : FailedResult
+    {
+        public FailedInvalidParamResult(string objectName)
+        {
+            ErrorCode = $"{objectName}{ErrorContants.InvalidCode}";
+            ErrorMessage = $"{objectName} {ErrorContants.Invalid}";
+        }
+    }
+
+    public class FailedInvalidParamResult<T> : FailedResult<T>
+    {
+        public FailedInvalidParamResult(string objectName)
+        {
+            ErrorCode = $"{objectName}{ErrorContants.InvalidCode}";
+            ErrorMessage = $"{objectName} {ErrorContants.Invalid}";
+        }
+    }
+
+    public class WarningResult<T> : Result<T>
+    {
+        public WarningResult() : base()
+        {
+            Warning = true;
+            Succeeded = false;
+        }
+
+        public WarningResult(string warningCode, string warningMessage) : base()
+        {
+            Warning = true;
+            Succeeded = false;
+            ErrorCode = warningCode;
+            ErrorMessage = warningMessage;
+        }
+    }
+}
